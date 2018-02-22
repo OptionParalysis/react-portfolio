a78dec6d0bf06e41c550868284ca102a6bf07ae8
OptionParalysis <zjanice@imodules.com>
2018-02-22 12:15:54 -0600

    setup proxy for cors issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 // behance
 const USER_ID = 'zachjanice';
 const API_KEY = '?api_key=3uFNmciSUWV7Cc1j366WsDxh7jg8q97H';
-const PROJECTS_URL = 'https://api.behance.net/v2/users/';
-const PROJECT_URL = 'https://www.behance.net/v2/projects/';
+const PROJECTS_URL = 'https://www.behance.net/v2/users/';
+const PROJECT_URL = 'https://api.behance.net/v2/projects/';
 
 export const FETCH_PROJECTS = 'fetch_projects';
 export const FETCH_PROJECT = 'fetch_project';
 export const FETCH_PROJECTS_ERR = 'fetch_projects_err';
 export const FETCH_PROJECT_ERR = 'fetch_project_err';
+
+// proxy for cors
+const proxyurl = 'https://cors-anywhere.herokuapp.com/';
 
 export const CREATE_CONTACT = 'create_contact';
 
@@ -31,7 +34,7 @@ export function fetchProjects() {
   const url = `${PROJECTS_URL}${USER_ID}/projects${API_KEY}`;
 
   return function(dispatch) {
-    axios.get(url)
+    axios.get(proxyurl + url)
       .then((response) => {
         dispatch(fetchProjectsSuccess(response))
       })
@@ -60,7 +63,7 @@ export function fetchProject(id) {
   const url = `${PROJECT_URL}${id}${API_KEY}`;
   
   return function(dispatch) {
-    axios.get(url)
+    axios.get(proxyurl + url)
       .then((response) => {
         dispatch(fetchProjectSuccess(response))
       })
