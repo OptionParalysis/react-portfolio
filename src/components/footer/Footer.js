e593fc86145cafa5dfd3ffd93d2a08c91899e6fc
OptionParalysis <zjanice@imodules.com>
2018-02-22 20:32:29 -0600

    update disclaimer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './footer.css';
 
 const Footer = () => (
   <footer>
-    <p>Disclaimer</p>
+    <p>All work displayed, I am the author of.  I don't claim copyright for any work displayed unless stated otherwise.  Some of this work is from past/current employers in which I either design and/or coded.  Long story short, this is my little gallery.  Please contact me if there are questions or concerns.</p>
   </footer>
 );
 
