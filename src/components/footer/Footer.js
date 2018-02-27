c51e82dcd994349180804a5964e136f4f1117933
OptionParalysis <zjanice@imodules.com>
2018-02-26 21:29:05 -0600

    update disclaimer and colors for wcag

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 
 const Footer = () => (
   <footer>
-    <p>All work displayed, I am the author and/or co-creator of.  I don't claim copyright for any work displayed unless stated otherwise. Please contact me if there are questions or <Link to="/contact">concerns</Link>.</p>
+    <p>All work displayed, I am the author and/or co-creator of.  I don't claim copyright for any work displayed unless stated otherwise. Please <Link to="/contact">contact me</Link> if there are questions or concerns.</p>
   </footer>
 );
 
