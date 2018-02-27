d987a247d50a26a784cab648908a55b303f83c2b
OptionParalysis <zjanice@imodules.com>
2018-02-26 21:22:15 -0600

    update disclaimer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './footer.css';
 
 const Footer = () => (
   <footer>
-    <p>All work displayed, I am the author of.  I don't claim copyright for any work displayed unless stated otherwise.  Some of this work is from past/current employers in which I either design and/or coded.  Long story short, this is my little gallery.  Please contact me if there are questions or concerns.</p>
+    <p>All work displayed, I am the author and/or co-creator of.  I don't claim copyright for any work displayed unless stated otherwise. Please contact me if there are questions or <Link to="/contact">concerns</Link>.</p>
   </footer>
 );
 
